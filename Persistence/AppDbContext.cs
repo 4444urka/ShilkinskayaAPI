241e13a977b0f1e5b767292a230e7999c376b3f3
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-28 15:42:27 +1000

    add Infrastructure and Persistence projects to solution

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    
+    
+}
